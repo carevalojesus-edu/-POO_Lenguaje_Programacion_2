5e7e42bebd8f7b58655c1ca3c51d3d38e1aba5d4
Christian Arevalo Jesus <christian.arevalo@unapiquitos.edu.pe>
2024-09-26 19:03:19 -0500

    Clase persona

---
+++ src/01_clases_objetos/Persona.java
@@ -0,0 +1,25 @@
+public class Persona {
+    // Atributos de la clase Persona
+    private String nombre;
+    private int edad;
+
+    // Constructor de la clase Persona
+    public Persona(String nombres, int edad){
+        this.nombre = nombres;
+        this.edad = edad;
+    }
+
+    // Método para mostrar la informacion de la Persona
+    public void mostrarInformacion(){
+        System.out.println("Nombres: " + this.nombre + " Edad: " + this.edad);
+    }
+
+    // Método para saludar
+    public void saludar(){
+        if (this.edad > 1 ) {
+            System.out.println("Hola, me llamo " + this.nombre + " y tengo " + this.edad + " años");
+        } else {
+            System.out.println("Hola, me llamo " + this.nombre + " y tengo " + this.edad + " año");
+        }
+    }
+}
